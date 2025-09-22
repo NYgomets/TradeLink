a20c50034907027c34676f7d8a5fb8674c67efc4
ParkJuhyeong <126853427+NYgomets@users.noreply.github.com>
2025-09-22 09:33:16 +0900

    feat:고아 게시글 및 파일 삭제 스케줄러 기능 추가-#13 (#14)

--- tradelink/src/main/java/site/tradelink/tradelink/post/service/file/NcpS3Service.java
+++ tradelink/src/main/java/site/tradelink/tradelink/post/service/file/NcpS3Service.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -60,16 +61,16 @@ public class NcpS3Service {
         return failureKeys;
     }
 
-    public List<S3Object> listAllObjectsByPrefix(String prefix) {
-        try {
-            ListObjectsV2Request listReq = ListObjectsV2Request.builder()
-                    .bucket(bucket)
-                    .prefix(prefix)
-                    .build();
+    /**
+     * S3 객체 목록을 페이지네이션하여 가져오는 Iterable을 반환
+     * 대용량 데이터를 메모리 문제 없이 처리하기 위해 사용
+     */
+    public ListObjectsV2Iterable listObjectsByPrefixPaginated(String prefix) {
+        ListObjectsV2Request listReq = ListObjectsV2Request.builder()
+                .bucket(bucket)
+                .prefix(prefix)
+                .build();
 
-            return s3Client.listObjectsV2(listReq).contents();
-        } catch (S3Exception e) {
-            return Collections.emptyList();
-        }
+        return s3Client.listObjectsV2Paginator(listReq);
     }
 }
